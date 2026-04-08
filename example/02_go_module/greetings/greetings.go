2073dc6683dae3298dc2845ac74822d3244184bf
Trung Nguyen <trung.ng@embedcard.com>
2026-04-08 21:36:32 +0800

    go error hanlder

--- example/02_go_module/greetings/greetings.go
+++ example/02_go_module/greetings/greetings.go
@@ -1,10 +1,30 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
-    // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message
+func Hello(name string) (string, error) {
+
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// create a message from random format
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+func randomFormat() string {
+	// a slice of messages format
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
--- example/02_go_module/hello/hello.go
+++ example/02_go_module/hello/hello.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+
+	// set log prefix and flag 0 (disable printing the time, source file, line number)
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("Noi")
+	// if returing an error, print to console and exit
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
